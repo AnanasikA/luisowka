2c27354989b677c4c137dac62a50c5640a022159
AnanasikA <valievaanastasia98@gmail.com>
2025-07-26 16:15:09 +0200

    Poprawki strony Luiswka – wersja dziaajca

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  trailingSlash: true,
-  basePath: '/luisowka',
+
 };
 
 module.exports = nextConfig;
